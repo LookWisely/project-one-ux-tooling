76de8de236afc8ace29edfe9c923462c9e1154ee
Aakankshagarg <39582626+Aakankshagarg@users.noreply.github.com>
2020-03-27 11:12:52 -0400

    Part A and B

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  // var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+}
+
 let mainNavDot = document.querySelectorAll(`.navbar-nav li a`);
 let mainSection = document.querySelectorAll(`.chapter`);
 
@@ -19,3 +48,4 @@ window.addEventListener(`scroll`, event => {
       }
     });
   });
+
